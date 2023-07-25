48d6be658bf8114c0422b2684d6333343fe75a5a
adamaconguli <waltson2003@gmail.com>
2023-07-25 20:08:51 +0800

    Lesson 4

--- FirstReactPage(Static)/index.js
+++ FirstReactPage(Static)/index.js
@@ -1,1 +1,11 @@
+// Lesson 3
 ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+
+// Lesson 4
+ReactDOM.render(
+	<ul>
+		<li>Thing 1</li>
+		<li>Thing 2</li>
+	</ul>,
+	document.getElementById("root")
+);
