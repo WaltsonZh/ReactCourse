ba082b04fc177eccb7106012c5fd56769d9001b0
adamaconguli <waltson2003@gmail.com>
2023-07-26 14:41:31 +0800

    lesson 8

--- FirstReactPage(Static)/index.js
+++ FirstReactPage(Static)/index.js
@@ -27,3 +27,17 @@ const h1 = document.createElement("h1");
 h1.textContent = "This is an imperative way to program";
 h1.className = "header";
 document.getElementById("root").append(h1);
+
+// Lesson 8
+const navbar = (
+	<nav>
+		<h1>Bob's Bistro</h1>
+		<ul>
+			<li>Menu</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
