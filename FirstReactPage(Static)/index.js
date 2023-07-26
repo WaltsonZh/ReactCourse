fa186cf1cd7f3a1bede4c0f1df970f5326e78293
adamaconguli <waltson2003@gmail.com>
2023-07-26 14:26:56 +0800

    lesson 7

--- FirstReactPage(Static)/index.js
+++ FirstReactPage(Static)/index.js
@@ -21,3 +21,9 @@ ReactDOM.render(
 	</div>,
 	document.getElementById("root")
 );
+
+// Lesson 7
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
