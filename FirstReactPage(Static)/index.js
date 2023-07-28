9293be30540a676fea04dd01fbbb8a94346031cb
adamaconguli <waltson2003@gmail.com>
2023-07-28 15:35:25 +0800

    lesson 14

--- FirstReactPage(Static)/index.js
+++ FirstReactPage(Static)/index.js
@@ -58,3 +58,23 @@ const page = (
 
 ReactDOM.render(page, document.getElementById("root"));
 // document.getElementById("root").append(JSON.stringify(page))
+
+// Lesson 14
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+const page1 = (
+	<div>
+		<img src="./react-logo.png" width="40px" />
+		<h1>Fun facts about React</h1>
+		<ul>
+			<li>Was first released in 2013</li>
+			<li>Was originally created by Jordan Walke</li>
+			<li>Has well over 100K stars on GitHub</li>
+			<li>Is maintained by Facebook</li>
+			<li>Powers thousands of enterprise apps, including mobile apps</li>
+		</ul>
+	</div>
+);
+
+ReactDOM.render(page1, document.getElementById("root"));
