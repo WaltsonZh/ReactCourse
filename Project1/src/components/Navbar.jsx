29261d01b8ac1f753f72d94a4525c838da41f1eb
adamaconguli <waltson2003@gmail.com>
2023-07-31 18:30:35 +0800

    lesson 26 -- Navbar

--- Project1/src/components/Navbar.jsx
+++ Project1/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "/src/images/react-icon.png";
 
 export default function Navbar() {
-	return (
-		<nav>
-			<h3>ReactFacts</h3>
-		</nav>
-	);
+    return (
+        <nav className="Navbar">
+            <img src={logo} className="nav-icon" />
+            <h3 className="nav-iconText">ReactFacts</h3>
+            <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
+    );
 }
