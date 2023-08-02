4d2f1dbce247d793985993a65ced66890cf3dfff
adamaconguli <waltson2003@gmail.com>
2023-08-02 17:49:18 +0800

    lesson 3: Hero components

---
+++ Airbnb-clone/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <section className='Hero'>
+            <img src='/src/images/photo-grid.png' width='396px' />
+            <div className='Hero-text'>
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </section>
+    )
+}
