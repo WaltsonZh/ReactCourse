9639c543e3ed2b14fb6f3d023301f94f7e382950
adamaconguli <waltson2003@gmail.com>
2023-08-02 16:51:19 +0800

    lesson 2: Navbar

---
+++ Airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='Navbar'>
+            <img src='/src/images/airbnb-logo.png' width='82px' />
+        </nav>
+    )
+}
