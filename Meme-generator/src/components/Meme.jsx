764fe3c37eaf6ea0996840667eb21484cbce7e7e
adamaconguli <waltson2003@gmail.com>
2023-08-07 18:00:32 +0800

    lesson 6: get memes

--- Meme-generator/src/components/Meme.jsx
+++ Meme-generator/src/components/Meme.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import memesData from '../../public/memesData'
 
 export default function Meme() {
+
+    function getImage() {
+        let URL = memesData.data.memes[Math.floor(Math.random() * 100)].url
+        console.log(URL)
+    }
+
     return (
         <section className='Meme'>
             <input type='text' placeholder='Top Text' />
             <input type='text' placeholder='Bottom Text' />
-            <button>Get a new meme image 🖼️</button>
+            <button onClick={getImage}>Get a new meme image 🖼️</button>
         </section>
     )
 }
