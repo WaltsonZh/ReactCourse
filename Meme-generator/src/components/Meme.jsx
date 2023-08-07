bd1db5a133d0ed1077e9745171792439894d2f95
adamaconguli <waltson2003@gmail.com>
2023-08-07 17:23:08 +0800

    lesson 3: form

---
+++ Meme-generator/src/components/Meme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Meme() {
+    return (
+        <section className='Meme'>
+            <input type='text' placeholder='Top Text' />
+            <input type='text' placeholder='Bottom Text' />
+            <button>Get a new meme image 🖼️</button>
+        </section>
+    )
+}
