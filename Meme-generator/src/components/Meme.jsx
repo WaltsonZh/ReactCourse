54e3aefab8c710f19c416ed7639a8469e5e6c659
adamaconguli <waltson2003@gmail.com>
2023-08-11 15:06:10 +0800

    lesson 17: add image to meme generator

--- Meme-generator/src/components/Meme.jsx
+++ Meme-generator/src/components/Meme.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import memesData from '../../public/memesData'
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = useState('https://i.imgflip.com/3si4.jpg')
 
     function getImage() {
         let URL = memesData.data.memes[Math.floor(Math.random() * 100)].url
-        console.log(URL)
+        setMemeImage(URL)
     }
 
     return (
-        <section className='Meme'>
-            <input type='text' placeholder='Top Text' />
-            <input type='text' placeholder='Bottom Text' />
-            <button onClick={getImage}>Get a new meme image 🖼️</button>
+        <section>
+            <div className='Meme'>
+                <input type='text' placeholder='Top Text' />
+                <input type='text' placeholder='Bottom Text' />
+                <button onClick={getImage}>Get a new meme image 🖼️</button>
+            </div>
+            <img src={memeImage} className='memeImg' />
         </section>
     )
 }
