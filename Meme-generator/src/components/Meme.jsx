c0722f4080045b3946ef5014f4caeadf0454b932
adamaconguli <waltson2003@gmail.com>
2023-08-14 16:07:15 +0800

    lesson 52: Add text to image

--- Meme-generator/src/components/Meme.jsx
+++ Meme-generator/src/components/Meme.jsx
@@ -5,27 +5,39 @@ export default function Meme() {
     const [meme, setMeme] = useState({
         topText: '',
         bottomText: '',
-        randomImage: 'https://i.imgflip.com/3si4.jpg'
+        randomImage: 'https://i.imgflip.com/3si4.jpg',
     })
 
     const [allMemeImages, setAllMemeImages] = useState(memesData)
 
     function getImage() {
         let URL = allMemeImages.data.memes[Math.floor(Math.random() * 100)].url
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            randomImage: URL,
+        }))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
-            randomImage: URL
+            [name]: value
         }))
     }
 
     return (
         <section>
-            <div className='Meme'>
-                <input type='text' placeholder='Top Text' />
-                <input type='text' placeholder='Bottom Text' />
+            <div className='form'>
+                <input type='text' placeholder='Top Text' name='topText' onChange={handleChange} value={meme.topText} />
+                <input type='text' placeholder='Bottom Text' name='bottomText' onChange={handleChange} value={meme.bottomText} />
                 <button onClick={getImage}>Get a new meme image 🖼️</button>
             </div>
-            <img src={meme.randomImage} className='memeImg' />
+            <div className='meme'>
+                <img src={meme.randomImage} className='memeImg' />
+                <h2 className='memeText top'>{meme.topText}</h2>
+                <h2 className='memeText bottom'>{meme.bottomText}</h2>
+            </div>
         </section>
     )
 }
